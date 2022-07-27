70857b9f3696350d19dc94ac44dec822609d1873
unicorndolphin123 <99698392+unicorndolphin123@users.noreply.github.com>
2022-07-27 18:54:10 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -8,5 +8,23 @@ recognition.onresult = function(event){
     console.log(event);
     var Content = event.results [0] [0].transcript;
     console.log(Content);
+    speak();
     document.getElementById("textbox").innerHTML = Content;
 }
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data = document.getElementById("textbox").value;
+    var UtterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(UtterThis);
+    Webcam.attach(camera);
+}
+
+Webcam.set({
+    width:360,
+    height:250,
+    image_format : 'png',
+    png_quality:90
+});
+camera = document.getElementById("camera");
+
